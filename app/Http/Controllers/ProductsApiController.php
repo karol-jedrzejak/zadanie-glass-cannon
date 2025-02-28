914c096f6fceb769debf9df8dab66ba7a3d07718
karol-jedrzejak <karol.jedrzejak@gmail.com>
2025-02-28 10:30:12 +0100

    up

--- app/Http/Controllers/ProductsApiController.php
+++ app/Http/Controllers/ProductsApiController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 use App\Models\Products;
 
 use App\Http\Requests\ProductsStoreRequest;
--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,11 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Auth;
-
-use Illuminate\Http\Request;
-
 use Inertia\Inertia;
 
 class ProductsController extends Controller
